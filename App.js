526f54855d54f34d59667cb49337315750d778b3
zolomohan <mohanzolomon@gmail.com>
2021-01-04 13:29:42 +0530

    Install Dependencies & Bump up minSDKVersion

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Firebase Storage</Text>
+      <Button>Select Image</Button>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
